f88049795a42b076402a0e558dd0b77bc58e98f1
michael <michaelisaac372@gmaail.com>
2025-09-27 04:58:11 -0700

    extracted campaignForm zod and useForm  implimentation.  Add loading state to form button

---
+++ app/Campaigns/New/textFieldSlots.js
@@ -0,0 +1,9 @@
+import { InputAdornment } from "@mui/material";
+
+export const campaignTextFieldSlots = () => ({
+  input: {
+    endAdornment: (
+      <InputAdornment position="end">wei</InputAdornment>
+    ),
+  }
+});
